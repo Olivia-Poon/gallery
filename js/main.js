16728c95898a2389196036150eeefc7de71d7b42
Graphic <graphic@SOD-SJH-516-10M.local>
2015-11-06 14:59:15 -0500

    second

--- js/main.js
+++ js/main.js
@@ -1,1 +1,29 @@
-#hi
+
+var resize= function(){
+	$(this).height($(this).width())
+};
+	$('.galimg').each(resize)
+
+
+
+$('.galimg').css('display','block');
+
+
+
+
+
+
+$('galimg:nth-child(1)').addClass('portrait');
+// $('galimg:nth-child(2)').addClass('landscape');
+// $('galimg:nth-child(3)').addClass('landscape');
+// $('galimg:nth-child(4)').addClass('landscape');
+// $('galimg:nth-child(5)').addClass('landscape');
+// $('galimg:nth-child(6)').addClass('landscape');
+// $('.gallery .galimg:last-child').addClass('landscape');
+
+// if ($('.galimg').width>$('.galimg').height){
+// 	$('.galimg').addClass('landscape')
+// }
+// else{
+// 	$('.galimg').addClass('portrait')
+// }
